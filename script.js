3308452da6a180ab06aee31ae64b5b92d84a050d
juliosilvar <juliosilvaregalado@gmail.com>
2025-05-16 12:06:32 -0500

    143

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let imagenes = [];
+let modalIndex = 0;
 
-function openModal(img) {
-    const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modal-img');
-    modal.style.display = 'block';
-    modalImg.src = img.src;
+document.addEventListener("DOMContentLoaded", () => {
+  imagenes = Array.from(document.querySelectorAll('.gallery-scroll img'));
+});
+
+function openModal(index) {
+  modalIndex = index;
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  modal.style.display = "flex";
+  modalImg.src = imagenes[modalIndex].src;
+}
+
+function cambiarImagen(offset) {
+  modalIndex = (modalIndex + offset + imagenes.length) % imagenes.length;
+  document.getElementById("modal-img").src = imagenes[modalIndex].src;
+}
+
+function cerrarModal() {
+  document.getElementById("modal").style.display = "none";
 }
